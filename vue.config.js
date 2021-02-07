ef4aa3310570b022a0d797414874617bf3163e73
backmeupplz <backmeupplz@gmail.com>
2021-02-07 07:53:40 -0800

    move config

--- vue.config.js
+++ vue.config.js
@@ -8,6 +8,12 @@ module.exports = {
         productName: 'Todorant',
         artifactName: 'todorant-${version}-${os}-${arch}.${ext}',
         publish: ['github'],
+        mac: {
+          hardenedRuntime: true,
+          entitlements: 'build/entitlements.mac.plist',
+          entitlementsInherit: 'build/entitlements.mac.plist',
+        },
+        afterSign: 'scripts/notarize.js',
       },
       preload: 'src/public/scripts/preloader.ts',
     },
